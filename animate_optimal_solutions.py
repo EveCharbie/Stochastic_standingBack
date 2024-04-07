f8a93159088950a752cc295069e01c15ac5c6425
Charbie <eve.charbonneau.1@umontreal.ca>
2024-04-07 14:25:35 -0400

    pyorerun (but does not allow saving videos)

--- animate_optimal_solutions.py
+++ animate_optimal_solutions.py
@@ -1,32 +1,34 @@
 import pickle
 
-import sys
-sys.path.append("/home/charbie/Documents/Programmation/pyorerun/")
-from pyorerun import BiorbdModel, MultiPhaseRerun
+# sys.path.append("/home/charbie/Documents/Programmation/pyorerun/")
+# from pyorerun import BiorbdModel, MultiPhaseRerun
 
+def noisy_animate_rerun(model_name, model_name_mean, q_integrated, time_vector, q_mean_last, animation_name):
 
-def noisy_animate(model_name, q_integrated, time_vector, q_mean_last, animation_name):
+    # Options
+    display_model_options = DisplayModelOptions()
 
-    biorbd_model = BiorbdModel(model_name)
+    biorbd_model = BiorbdModel(model_name, display_model_options=DisplayModelOptions())
     rerun_biorbd = MultiPhaseRerun()
 
     rerun_biorbd.add_phase(t_span=time_vector, phase=0, window="animation")
     for i_random in range(q_integrated.shape[2]):
         rerun_biorbd.add_animated_model(biorbd_model, q_integrated[:, :, i_random], phase=0, window="animation")
 
-    red_model = BiorbdModel(model_name)
-    red_model.options.mesh_color = (1, 0, 0)
+    mean_model = BiorbdModel(model_name_mean)
+    mean_model.options.mesh_color = (1, 1, 1)
 
-    rerun_biorbd.add_animated_model(red_model, q_mean_last, phase=0, window="animation")
+    rerun_biorbd.add_animated_model(mean_model, q_mean_last, phase=0, window="animation")
     rerun_biorbd.rerun(animation_name)
-
 
 
 model_name = "Model2D_7Dof_0C_3M"
 biorbd_model_path = f"models/{model_name}.bioMod"
 biorbd_model_path_with_mesh = f"models/{model_name}_with_mesh.bioMod"
+biorbd_model_path_with_mesh_mean = f"models/{model_name}_with_mesh_mean.bioMod"
 biorbd_model_path_vision = f"models/{model_name}_vision.bioMod"
 biorbd_model_path_vision_with_mesh = f"models/{model_name}_vision_with_mesh.bioMod"
+biorbd_model_path_vision_with_mesh_mean = f"models/{model_name}_vision_with_mesh_mean.bioMod"
 
 ocp_path_to_results = f"results/good/{model_name}_ocp_DMS_CVG_1e-8.pkl"
 socp_path_to_results = f"TODO"
@@ -55,7 +57,7 @@ print_tol = "{:.1e}".format(tol)
 
 # OCP
 ocp_out_path_to_results = ocp_path_to_results.replace(".pkl", "_integrated.pkl")
-with open(ocp_path_to_results, "rb") as file:
+with open(ocp_out_path_to_results, "rb") as file:
     data = pickle.load(file)
     q_ocp_integrated = data["q_integrated"]
     qdot_ocp_integrated = data["qdot_integrated"]
@@ -65,4 +67,5 @@ with open(ocp_path_to_results, "rb") as file:
     time_vector = data["time_vector"]
     q_nominal = data["q_nominal"]
 
-noisy_animate(biorbd_model_path_with_mesh, q_ocp_integrated, time_vector, q_nominal, "OCP")
+time_vector_animate = time_vector * 10
+noisy_animate(biorbd_model_path_with_mesh, biorbd_model_path_with_mesh_mean, q_ocp_integrated, time_vector_animate, q_nominal, "OCP")
