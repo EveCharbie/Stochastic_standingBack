6e1ed9ab6c375a1303b3ced185876e5589b19934
Charbie <eve.charbonneau.1@umontreal.ca>
2026-04-02 11:34:12 -0400

    animations with eyes

--- main_DMS.py
+++ main_DMS.py
@@ -15,11 +15,11 @@ from DMS_SOCP_VARIABLE import prepare_socp_VARIABLE
 from DMS_SOCP_FEEDFORWARD import prepare_socp_FEEDFORWARD
 from DMS_SOCP_VARIABLE_FEEDFORWARD import prepare_socp_VARIABLE_FEEDFORWARD
 
-RUN_OCP = False
+RUN_OCP = True
 RUN_SOCP = False
 RUN_SOCP_VARIABLE = False
-RUN_SOCP_FEEDFORWARD = True
-RUN_SOCP_VARIABLE_FEEDFORWARD = True
+RUN_SOCP_FEEDFORWARD = False
+RUN_SOCP_VARIABLE_FEEDFORWARD = False
 print(RUN_OCP, RUN_SOCP, RUN_SOCP_VARIABLE, RUN_SOCP_FEEDFORWARD, RUN_SOCP_VARIABLE_FEEDFORWARD)
 print(datetime.now().strftime("%d-%m %H:%M:%S"))
 
@@ -124,10 +124,10 @@ if RUN_OCP:
         pickle.dump(sol_ocp, file)
 
     print(save_path)
-    # import bioviz
-    # b = bioviz.Viz(model_path=biorbd_model_path_with_mesh)
-    # b.load_movement(np.vstack((q_roots_sol, q_joints_sol)))
-    # b.exec()
+    import bioviz
+    b = bioviz.Viz(model_path=biorbd_model_path_with_mesh)
+    b.load_movement(np.vstack((q_roots_sol, q_joints_sol)))
+    b.exec()
 else:
     save_path = save_path.replace(".", "p")
     save_path = save_path.replace("ppkl", f"_DVG_1e-8.pkl")
